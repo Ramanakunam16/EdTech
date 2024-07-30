fd2dc156f892948925e6cda4a48c5418cde2e7fb
ramanakunam16 <ramanakunam16@gmail.com>
2024-07-30 12:51:45 +0530

    updated database connection

--- startup/dbConnection.js
+++ startup/dbConnection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const config = require("../utils/config");
 
-const url = `mongodb+srv://ramanakunam16:${config.MONGODB_PASSWORD}@cluster0.q0virzs.mongodb.net/`;
+const url = `mongodb+srv://ramanakunam16:${config.MONGODB_PASSWORD}@cluster0.q0virzs.mongodb.net/edtech`;
 
 async function connection() {
   await mongoose
