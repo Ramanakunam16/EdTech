0c320a97c60ef73f06c3e9eecdd51afd36ba3d4d
ramanakunam16 <ramanakunam16@gmail.com>
2024-07-30 12:48:48 +0530

    updated database connection

--- startup/dbConnection.js
+++ startup/dbConnection.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
+const config = require("../utils/config");
 
-const url = "mongodb://localhost:27017/edtech";
+const url = `mongodb+srv://ramanakunam16:${config.MONGODB_PASSWORD}@cluster0.q0virzs.mongodb.net/`;
 
 async function connection() {
   await mongoose
--- utils/config.js
+++ utils/config.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const PORT = process.env.PORT;
 const JWT_PRIVATE_KEY = process.env.JWT_PRIVATE_KEY;
+const MONGODB_PASSWORD = process.env.MONGODB_PASSWORD;
 
-module.exports = { PORT, JWT_PRIVATE_KEY };
+module.exports = { PORT, JWT_PRIVATE_KEY, MONGODB_PASSWORD };
